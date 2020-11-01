484972d0db601d8113630553cf6235a34b7f3fc6
LoomCai <111377274+LoomCai@users.noreply.github.com>
2020-11-01 12:55:16 +0000

    minor reformat

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -12,4 +12,10 @@
 # All configuration values have a default; values that are commented out
 # serve to show the default.
 
-import s
+import sys
+import os
+import shlex
+
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the direc
