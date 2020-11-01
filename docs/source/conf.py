4d5865fcb587527685f0263cd47ffbec59f8019c
LoomCai <111377274+LoomCai@users.noreply.github.com>
2020-11-01 07:28:11 +0000

    Add license info.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,3 +2,8 @@
 #
 # saws documentation build configuration file, created by
 # sphinx-quickstart on Fri Sep 11 16:09:11 2015.
+#
+# This file is execfile()d with the current directory set to its
+# containing dir.
+#
+# Note that not all possible configuration values are present in t
