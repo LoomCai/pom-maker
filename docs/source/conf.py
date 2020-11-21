228d1f2c23e9b6bd4d5032a4818f93fa2c4b95a8
LoomCai <111377274+LoomCai@users.noreply.github.com>
2020-11-21 12:22:57 +0000

    Include problem testcases

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -66,4 +66,7 @@ release = '0.1.0'
 # for a list of supported languages.
 #
 # This is also used if you do content translation via gettext catalogs.
-# Usually you 
+# Usually you set "language" from the command line for these cases.
+language = None
+
+# There are two options for replacing |today|: either, you set to
