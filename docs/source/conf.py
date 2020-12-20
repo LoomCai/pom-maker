0928aca98c404a9dca8c8060661e50396ba24a88
LoomCai <111377274+LoomCai@users.noreply.github.com>
2020-12-20 16:31:31 +0000

    Ups

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -139,4 +139,7 @@ html_theme = 'alabaster'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
-# so a file
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+# Add any extra paths that contain custom
