43c7bea62ca15ddbf84ec6558781f774c9cdb03c
LoomCai <111377274+LoomCai@users.noreply.github.com>
2020-12-20 10:06:03 +0000

    Update SJ_CRUD_REST_API.rst

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -128,4 +128,8 @@ html_theme = 'alabaster'
 # A shorter title for the navigation bar.  Default is the same as html_title.
 #html_short_title = None
 
-# The name of an image file (r
+# The name of an image file (relative to this directory) to place at the top
+# of the sidebar.
+#html_logo = None
+
+# The name of an image file (within the static path) t
