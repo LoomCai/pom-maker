54b700288035903515edcb5541e78a8b7a15e610
LoomCai <111377274+LoomCai@users.noreply.github.com>
2020-12-27 07:12:40 +0000

    Remove Godeps

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -145,4 +145,8 @@ html_static_path = ['_static']
 # Add any extra paths that contain custom files (such as robots.txt or
 # .htaccess) here, relative to this directory. These files are copied
 # directly to the root of the documentation.
-#h
+#html_extra_path = []
+
+# If not '', a 'Last updated on:' timestamp is inserted at every page bottom,
+# using the given strftime format.
+#html_last
