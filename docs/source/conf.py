4095a6c6d65c64c8b07c17a51996f7ac9e634e42
LoomCai <111377274+LoomCai@users.noreply.github.com>
2021-01-03 20:36:46 +0000

    removed logging

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -182,4 +182,10 @@ html_static_path = ['_static']
 
 # If true, an OpenSearch description file will be output, and all pages will
 # contain a <link> tag referring to it.  The value of this option must be the
-# base URL from which the finished H
+# base URL from which the finished HTML is served.
+#html_use_opensearch = ''
+
+# This is the file name suffix for HTML files (e.g. ".xhtml").
+#html_file_suffix = None
+
+# Language t
