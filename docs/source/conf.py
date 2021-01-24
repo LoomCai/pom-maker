1221c3ed30d33c7ff80e7905b4bbaf1ba3433894
LoomCai <111377274+LoomCai@users.noreply.github.com>
2021-01-24 06:18:58 +0000

    add TOC

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -233,4 +233,11 @@ latex_documents = [
 # the title page.
 #latex_logo = None
 
-# For "manual" documents, if this is true, then toplevel headings are parts
+# For "manual" documents, if this is true, then toplevel headings are parts,
+# not chapters.
+#latex_use_parts = False
+
+# If true, show page references after internal links.
+#latex_show_pagerefs = False
+
+# If true, show URL
