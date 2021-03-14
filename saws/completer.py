44ac5aaf6156d317813a73455fd3e8d9854cae5a
LoomCai <111377274+LoomCai@users.noreply.github.com>
2021-03-14 21:14:26 +0000

    drm/tilcdc: panel: fix dangling sysfs connector node

--- saws/completer.py
+++ saws/completer.py
@@ -1,3 +1,7 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2015 Donne Martin. All Right
+# Copyright 2015 Donne Martin. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License. A copy of
+# the Licens
