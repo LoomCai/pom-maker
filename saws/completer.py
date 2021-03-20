2652cebdfa447b867adedd64dc6d4055a881b77d
LoomCai <111377274+LoomCai@users.noreply.github.com>
2021-03-20 15:53:01 +0000

    Updated the Japanese translation

--- saws/completer.py
+++ saws/completer.py
@@ -11,4 +11,11 @@
 # or in the "license" file accompanying this file. This file is
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
-# language governing permissions and limita
+# language governing permissions and limitations under the License.
+
+from __future__ import unicode_literals
+from __future__ import print_function
+import re
+import sys
+import traceback
+from six.moves imp
