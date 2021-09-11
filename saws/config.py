c688ef6418b4ebea2bd7e373912f81ffbcd2ddb8
LoomCai <111377274+LoomCai@users.noreply.github.com>
2021-09-11 14:36:12 +0000

    Convert to C++

--- saws/config.py
+++ saws/config.py
@@ -1,1 +1,6 @@
-# -*- coding: 
+# -*- coding: utf-8 -*-
+
+# Copyright 2015 Donne Martin. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this
