e5abb5fd00011b6f033e51858a9b72ff20529681
LoomCai <111377274+LoomCai@users.noreply.github.com>
2021-11-27 09:07:46 +0000

    Release v1.1.1.

--- saws/keys.py
+++ saws/keys.py
@@ -76,3 +76,100 @@ class KeyManager(object):
         assert callable(refresh_resources_and_options)
         assert callable(handle_docs)
         self.manager = KeyBindingManager(
+            enable_search=True,
+            enable_abort_and_exit_bindings=True,
+            enable_system_bindings=True,
+            enable_auto_suggest_bindings=True)
+
+        @self.manager.registry.add_binding(Keys.F2)
+        def handle_f2(_):
+            """Enables/Disables color output.
+
+            Args:
+                * _: An instance of prompt_toolkit's Event (not used).
+
+            Returns:
+                None.
+            """
+            set_color(not get_color())
+
+        @self.manager.registry.add_binding(Keys.F3)
+        def handle_f3(_):
+            """Enables/Disables fuzzy matching.
+
+            Args:
+                * _: An instance of prompt_toolkit's Event (not used).
+
+            Returns:
+                None.
+            """
+            set_fuzzy_match(not get_fuzzy_match())
+
+        @self.manager.registry.add_binding(Keys.F4)
+        def handle_f4(_):
+            """Enables/Disables shortcut matching.
+
+            Args:
+                * _: An instance of prompt_toolkit's Event (not used).
+
+            Returns:
+                None.
+            """
+            set_shortcut_match(not get_shortcut_match())
+
+        @self.manager.registry.add_binding(Keys.F5)
+        def handle_f5(event):
+            """Refreshes AWS resources.
+
+            Args:
+                * event: An instance of prompt_toolkit's Event.
+
+            Returns:
+                None.
+            """
+            event.cli.run_in_terminal(refresh_resources_and_options)
+
+        @self.manager.registry.add_binding(Keys.F9)
+        def handle_f9(_):
+            """Inputs the "docs" command when the `F9` key is pressed.
+
+            Args:
+                * _: An instance of prompt_toolkit's Event (not used).
+
+            Returns:
+                None.
+            """
+            handle_docs(from_fkey=True)
+
+        @self.manager.registry.add_binding(Keys.F10)
+        def handle_f10(_):
+            """Quits when the `F10` key is pressed.
+
+            Args:
+                * _: An instance of prompt_toolkit's Event (not used).
+
+            Returns:
+                None.
+            """
+            raise EOFError
+
+        @self.manager.registry.add_binding(Keys.ControlSpace)
+        def handle_ctrl_space(event):
+            """Initializes autocompletion at the cursor.
+
+            If the autocompletion menu is not showing, display it with the
+            appropriate completions for the context.
+
+            If the menu is showing, select the next completion.
+
+            Args:
+                * event: An instance of prompt_toolkit's Event.
+
+            Returns:
+                None.
+            """
+            b = event.cli.current_buffer
+            if b.complete_state:
+                b.complete_next()
+            else:
+                event.cli.start_completion(select_first=False)
