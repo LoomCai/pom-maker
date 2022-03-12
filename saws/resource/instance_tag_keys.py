162714340b6901d82d2b0f8cae8f9f2d3cc3e2d6
LoomCai <111377274+LoomCai@users.noreply.github.com>
2022-03-12 13:57:49 +0000

    responsive layout

--- saws/resource/instance_tag_keys.py
+++ saws/resource/instance_tag_keys.py
@@ -12,3 +12,11 @@
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
 # language governing permissions and limitations under the License.
+
+from __future__ import unicode_literals
+from __future__ import print_function
+from .resource import Resource
+
+
+class InstanceTagKeys(Resource):
+    """E
