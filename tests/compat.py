5e083230e604cb3d2573fbf85b1fa77c02a23d55
LoomCai <111377274+LoomCai@users.noreply.github.com>
2022-09-17 05:27:55 +0000

    Added build badge

--- tests/compat.py
+++ tests/compat.py
@@ -1,3 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Copyright 2015 Donne Martin. All Rights Reserve
+# Copyright 2015 Donne Martin. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License"). You
+# may not use this file except in compliance with the License
