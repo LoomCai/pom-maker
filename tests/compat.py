ad45d55c005f0ccebb12f3da6d0829315d774798
LoomCai <111377274+LoomCai@users.noreply.github.com>
2022-09-18 14:50:17 +0000

    rebuilt site

--- tests/compat.py
+++ tests/compat.py
@@ -3,4 +3,9 @@
 # Copyright 2015 Donne Martin. All Rights Reserved.
 #
 # Licensed under the Apache License, Version 2.0 (the "License"). You
-# may not use this file except in compliance with the License
+# may not use this file except in compliance with the License. A copy of
+# the License is located at
+#
+#     http://aws.amazon.com/apache2.0/
+#
+# or in the "license" file accompanying this file. This fil
