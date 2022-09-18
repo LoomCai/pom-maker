805ad8c533c08a08e3d00700b72f9b45710536bb
LoomCai <111377274+LoomCai@users.noreply.github.com>
2022-09-18 16:45:27 +0000

    warning fix

--- tests/compat.py
+++ tests/compat.py
@@ -8,4 +8,6 @@
 #
 #     http://aws.amazon.com/apache2.0/
 #
-# or in the "license" file accompanying this file. This fil
+# or in the "license" file accompanying this file. This file is
+# distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
+# ANY KIND, either express or implied. See the Licens
