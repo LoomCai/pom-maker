bb07eac60972061dee1737abe9d86c06678337b1
LoomCai <111377274+LoomCai@users.noreply.github.com>
2022-10-05 19:35:19 +0000

    adjust structure

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -11,4 +11,14 @@
 # or in the "license" file accompanying this file. This file is
 # distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF
 # ANY KIND, either express or implied. See the License for the specific
-# language governing permissions and limitations u
+# language governing permissions and limitations under the License.
+
+from __future__ import unicode_literals
+import unittest
+import pip
+import pexpect
+
+
+class CliTest(unittest.TestCase):
+
+    def test_run_c
