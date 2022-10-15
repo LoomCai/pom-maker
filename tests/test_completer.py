c353c8872752736014e63ff6860b9999d1ab0524
LoomCai <111377274+LoomCai@users.noreply.github.com>
2022-10-15 21:39:13 +0000

    Polish README

--- tests/test_completer.py
+++ tests/test_completer.py
@@ -93,3 +93,159 @@ class CompleterTest(unittest.TestCase):
                     'ecs',
                     'efs',
                     'elasticache',
+                    'elasticbeanstalk',
+                    'elastictranscoder',
+                    'elb',
+                    'emr']
+        self.verify_completions(commands, expected)
+
+    def test_ec2_subcommands(self):
+        commands = ['aws ec2 c']
+        expected = ['cancel-bundle-task',
+                    'cancel-conversion-task',
+                    'cancel-export-task',
+                    'cancel-import-task',
+                    'cancel-reserved-instances-listing',
+                    'cancel-spot-fleet-requests',
+                    'cancel-spot-instance-requests']
+        self.verify_completions(commands, expected)
+
+    def test_aws_command(self):
+        commands = ['a', 'aw']
+        expected = [AwsCommands.AWS_COMMAND]
+        self.verify_completions(commands, expected)
+
+    def test_global_options(self):
+        commands = ['aws -', 'aws --']
+        expected = self.saws \
+            .all_commands[AwsCommands.CommandType.GLOBAL_OPTIONS.value]
+        self.verify_completions(commands, expected)
+
+    def test_resource_options(self):
+        commands = ['aws ec2 describe-instances --',
+                    'aws s3api get-bucket-acl --',
+                    'aws emr list-clusters --']
+        expected = self.saws \
+            .all_commands[AwsCommands.CommandType.RESOURCE_OPTIONS.value]
+        self.verify_completions(commands, expected)
+
+    def test_shortcuts(self):
+        commands = ['aws ec2 ls',
+                    'aws emr ls',
+                    'aws elb ls',
+                    'aws dynamodb ls']
+        expected = ['aws ec2 describe-instances',
+                    'aws emr list-clusters',
+                    'aws elb describe-load-balancers',
+                    'aws dynamodb list-tables']
+        shortcuts = dict(zip(commands, expected))
+        for command, expect in shortcuts.items():
+            result = self.completer.replace_shortcut(command)
+            assert result == expect
+
+    def test_shortcuts_fuzzy(self):
+        self.completer.fuzzy_match = True
+        self.completer.shortcut_match = True
+        commands = ['aws ec2ls']
+        expected = ['ec2 ls --instance-ids']
+        self.verify_completions(commands, expected)
+        commands = ['aws ec2start']
+        expected = ['ec2 start-instances --instance-ids']
+        self.verify_completions(commands, expected)
+        commands = ['aws ec2stop']
+        expected = ['ec2 stop-instances --instance-ids']
+        self.verify_completions(commands, expected)
+        commands = ['aws ec2tagk']
+        expected = ['ec2 ls --ec2-tag-key']
+        self.verify_completions(commands, expected)
+        commands = ['aws ec2tagv']
+        expected = ['ec2 ls --ec2-tag-value']
+        self.verify_completions(commands, expected)
+
+    def test_substitutions(self):
+        command = 'aws ec2 ls --filters "Name=tag-key,Values=%s prod"'
+        expected = 'aws ec2 ls --filters "Name=tag-key,Values=prod"'
+        result = self.completer.replace_substitution(command)
+        assert result == expected
+        command = 'aws ec2 ls --ec2-tag-key Stack'
+        expected = \
+            'aws ec2 describe-instances --filters "Name=tag-key,Values=Stack"'
+        result = self.completer.replace_shortcut(command)
+        assert result == expected
+        command = 'aws ec2 ls --ec2-tag-value prod'
+        expected = \
+            'aws ec2 describe-instances --filters "Name=tag-value,Values=prod"'
+        result = self.completer.replace_shortcut(command)
+        assert result == expected
+
+    def test_substitutions_with_more_tokens(self):
+        command = 'aws ec2 ls --filters "Name=tag-key,Values=%s prod" ' \
+                  '| grep IpAddress'
+        expected = 'aws ec2 ls --filters "Name=tag-key,Values=prod" ' \
+                   '| grep IpAddress'
+        result = self.completer.replace_substitution(command)
+        assert result == expected
+        command = 'aws ec2 ls --ec2-tag-key Stack | grep IpAddress'
+        expected = 'aws ec2 describe-instances --filters ' \
+                   '"Name=tag-key,Values=Stack" | grep IpAddress'
+        result = self.completer.replace_shortcut(command)
+        assert result == expected
+        command = 'aws ec2 ls --ec2-tag-value prod | grep IpAddress'
+        expected = 'aws ec2 describe-instances --filters ' \
+                   '"Name=tag-value,Values=prod" | grep IpAddress'
+        result = self.completer.replace_shortcut(command)
+        assert result == expected
+
+    @mock.patch('saws.resources.print')
+    def test_refresh_resources_and_options(self, mock_print):
+        self.completer.refresh_resources_and_options(force_refresh=False)
+        mock_print.assert_called_with('Loaded resources from cache')
+
+    def test_instance_ids(self):
+        commands = ['aws ec2 ls --instance-ids i-b']
+        expected = ['i-b875ecc3', 'i-b51d05f4', 'i-b3628153']
+        instance_ids = self.completer.resources.resource_lists[
+            self.completer.resources.ResourceType.INSTANCE_IDS.value]
+        instance_ids.resources.extend(expected)
+        self.verify_completions(commands, expected)
+        commands = ['aws ec2 ls --instance-ids i-a']
+        expected = ['i-a51d05f4', 'i-a71bd617', 'i-a1637b56']
+        instance_ids.resources.extend(expected)
+        self.verify_completions(commands, expected)
+
+    def test_instance_ids_fuzzy(self):
+        self.completer.fuzzy_match = True
+        commands = ['aws ec2 ls --instance-ids a5']
+        expected = ['i-a875ecc3', 'i-a41d55f4', 'i-a3628153']
+        instance_ids = self.completer.resources.resource_lists[
+            self.completer.resources.ResourceType.INSTANCE_IDS.value]
+        instance_ids.resources.extend(expected)
+        self.verify_completions(commands, expected)
+
+    def test_instance_keys(self):
+        commands = ['aws ec2 ls --ec2-tag-key na']
+        expected = ['name', 'namE']
+        instance_tag_keys = self.completer.resources.resource_lists[
+            self.completer.resources.ResourceType.INSTANCE_TAG_KEYS.value]
+        instance_tag_keys.resources.extend(expected)
+        self.verify_completions(commands, expected)
+        commands = ['aws ec2 ls --ec2-tag-key Sta']
+        expected = ['Stack']
+        instance_tag_keys.resources.extend(expected)
+        self.verify_completions(commands, expected)
+
+    def test_instance_tag_values(self):
+        commands = ['aws ec2 ls --ec2-tag-value prod']
+        expected = ['production', 'production-blue', 'production-green']
+        instance_tag_values = self.completer.resources.resource_lists[
+            self.completer.resources.ResourceType.INSTANCE_TAG_VALUES.value]
+        instance_tag_values.resources.extend(expected)
+        self.verify_completions(commands, expected)
+        commands = ['aws ec2 ls --ec2-tag-value test']
+        expected = ['testing']
+        instance_tag_values.resources.extend(expected)
+        self.verify_completions(commands, expected)
+
+    def test_bucket_names(self):
+        commands = ['aws s3pi get-bucket-acl --bucket web-']
+        expected = ['web-server-logs', 'web-server-images']
